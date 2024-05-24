5bdb47f2124b6e45f4e786e1f5178141f96485f4
evinanil2 <123241343+evinanil2@users.noreply.github.com>
2024-05-24 14:16:37 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function b1()
+{
+    window.location = "index_1.html";
+}
+function b2()
+{
+    window.location = "index_2.html";
+}
+function b3()
+{
+    window.location = "index_3.html";
+}
+function b4()
+{
+    window.location = "index_4.html";
+}
+function b5()
+{
+    window.location = "index_5.html";
+}
